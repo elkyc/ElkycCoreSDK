47c70f62b2f7e41e1cd97d4413c1031e69e74b28
Alex Gurin <dn070287gav@gmail.com>
2021-05-26 00:01:20 +0300

    version 1.0.3

---
+++ ElkycCoreSDK.xcframework/ios-arm64_x86_64-simulator/ElkycCoreSDK.framework/Headers/ElkycCoreSDK.h
@@ -0,0 +1,19 @@
+//
+//  ElkycCoreSDK.h
+//  ElkycCoreSDK
+//
+//  Created by Alex Gurin on 3/8/21.
+//
+
+#import <Foundation/Foundation.h>
+#import "pycabcrypt.h"
+
+//! Project version number for ElkycCoreSDK.
+FOUNDATION_EXPORT double ElkycCoreSDKVersionNumber;
+
+//! Project version string for ElkycCoreSDK.
+FOUNDATION_EXPORT const unsigned char ElkycCoreSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElkycCoreSDK/PublicHeader.h>
+
+
